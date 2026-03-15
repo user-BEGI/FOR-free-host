ba7691e79018a0dcb1a484971f7cf09ef6e5d3ca
begibro <begibrooo@gmail.com>
2026-03-16 04:00:25 +0900

    please

--- burger/wsgi.py
+++ burger/wsgi.py
@@ -14,3 +14,12 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'burger.settings')
 
 application = get_wsgi_application()
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+try:
+    if not User.objects.filter(username='begi').exists():
+        User.objects.create_superuser('begi', 'begibrol7@gmail.com', 'admin123')
+        print("Superuser created: admin / admin123")
+except:
+    pass
