7d8bca1c8087fda9d43c5c43e37497bbd9a734a2
begibro <begibrooo@gmail.com>
2026-03-16 10:01:18 +0900

    salom

--- burger/settings.py
+++ burger/settings.py
@@ -121,6 +121,9 @@ import os
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
